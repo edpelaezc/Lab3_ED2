94fd4b886b71b5b53b289cc2bd6c6b0c1b9c5bf0
Emmanuel Alvarado <josue0ghost@gmail.com>
2020-03-04 10:58:13 -0600

    Arbol

--- Huffman/Huffman/Huffman.cs
+++ Huffman/Huffman/Huffman.cs
@@ -68,5 +68,51 @@ namespace Huffman
 				decode(root.Right, index, str);
 			}
 		}
+
+		public void BuildHuffman(string text)
+		{
+			Dictionary<char, int> freq = new Dictionary<char, int>();
+
+			for (int i = 0; i < text.Length; i++)
+			{
+				if (!freq.ContainsKey(text[i]))
+				{
+					freq.Add(text[i], 0);
+				}
+				freq.Add(text[i], freq[text[i]] + 1);
+			}
+
+			SortedList<int, Node> pList = new SortedList<int, Node>();
+
+			for (int i = 0; i < freq.Count; i++)
+			{
+				KeyValuePair<char, int> pair = freq.ElementAt(i);
+				pList.Add(pair.Value, new Node(pair.Key, pair.Value, null, null));
+			}
+
+			
+
+			while (pList.Count != 1)
+			{
+				KeyValuePair<int, Node> pair = pList.ElementAt(pList.Count - 1);
+				pList.RemoveAt(pList.Count - 1);
+				Node left = pair.Value;
+
+				pair = pList.ElementAt(pList.Count - 1);
+				pList.RemoveAt(pList.Count - 1);
+				Node right = pair.Value;
+
+				int sum = left.freq + right.freq;
+				pList.Add(sum, new Node('0', sum, left, right));
+
+			}
+			KeyValuePair<int, Node> root = pList.ElementAt(pList.Count - 1);
+			Node nRoot = root.Value;
+
+			Dictionary<char, string> huffCode = new Dictionary<char, string>();
+			encode(nRoot, "", huffCode);
+
+
+		}	
 	}
 }
